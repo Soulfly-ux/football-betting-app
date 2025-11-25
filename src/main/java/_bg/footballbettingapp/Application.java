1b9338b5dc159aa8c31809565f561bc02175fc32
Soulfly-ux <sirenar3171@gmail.com>
2025-11-25 13:00:59 +0100

    Initial commit

---
+++ src/main/java/_bg/footballbettingapp/Application.java
@@ -0,0 +1,13 @@
+package _bg.footballbettingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballBettingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballBettingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/_bg/footballbettingapp/ApplicationTests.java
@@ -0,0 +1,13 @@
+package _bg.footballbettingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballBettingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
