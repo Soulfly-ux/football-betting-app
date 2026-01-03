79b9c482ed4e4b676e8680d69e6502a0250353ec
Soulfly-ux <sirenar3171@gmail.com>
2026-01-03 13:01:44 +0100

    feat: implement match settlement and bet processing Added @Transactional logic to finish matches and update scores Integrated betService.settleBetForMatch(id) to automate bet results after a match ends Added validation to prevent finishing already finished or cancelled matches

--- src/main/java/_bg/footballbettingapp/match/service/MatchAdminService.java
+++ src/main/java/_bg/footballbettingapp/match/service/MatchAdminService.java
@@ -1,14 +1,20 @@
 package _bg.footballbettingapp.match.service;
 
+import _bg.footballbettingapp.bet.service.BetService;
+import _bg.footballbettingapp.exception.DomainException;
 import _bg.footballbettingapp.match.model.Match;
 import _bg.footballbettingapp.match.model.MatchStatus;
 import _bg.footballbettingapp.match.repository.MatchRepository;
+import _bg.footballbettingapp.web.dto.FinishMatchRequest;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @Slf4j
@@ -16,14 +22,41 @@ public class MatchAdminService {
 
 
     private final MatchRepository matchRepository;
+    private final BetService betService;
 
     @Autowired
-    public MatchAdminService(MatchRepository matchRepository) {
+    public MatchAdminService(MatchRepository matchRepository, BetService betService) {
         this.matchRepository = matchRepository;
+        this.betService = betService;
     }
 
 
     public List<Match> getAdminOpenMatches() {
         return matchRepository.findAllByMatchStatusInOrderByStartTimeAsc(List.of(MatchStatus.SCHEDULED, MatchStatus.IN_PROGRESS));
     }
+
+    @Transactional
+    public void finishMatch(UUID id, Integer homeGoals, Integer awayGoals) {
+        Match match = matchRepository.findById(id)
+                .orElseThrow(() -> new DomainException("Match not found with id: " + id));
+       if (match.getMatchStatus() == MatchStatus.FINISHED){
+           throw new DomainException("Match is already finished");
+       }
+
+        if (match.getMatchStatus() == MatchStatus.CANCELLED){
+            throw new DomainException("Match is already cancelled");
+        }
+
+
+        match.setHomeGoals(homeGoals);
+        match.setAwayGoals(awayGoals);
+        match.setMatchStatus(MatchStatus.FINISHED);
+
+        matchRepository.saveAndFlush(match);
+
+        betService.settleBetForMatch(id);
+
+
+    }
+
 }
--- src/main/java/_bg/footballbettingapp/web/AdminMatchesController.java
+++ src/main/java/_bg/footballbettingapp/web/AdminMatchesController.java
@@ -4,10 +4,13 @@ import _bg.footballbettingapp.match.model.Match;
 import _bg.footballbettingapp.match.service.MatchAdminService;
 import _bg.footballbettingapp.match.service.MatchService;
 import _bg.footballbettingapp.web.dto.FinishMatchRequest;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -53,5 +56,33 @@ public class AdminMatchesController {
          modelAndView.setViewName("admin-finish");
 
         return modelAndView;
+    }
+
+    @PutMapping("/{id}/finish")
+    public ModelAndView finishMatch(@PathVariable UUID id, @Valid FinishMatchRequest finishRequest, BindingResult bindingResult ) {
+
+
+
+
+        if (bindingResult.hasErrors()){
+
+            Match matchById = matchService.getMatchById(id);
+            ModelAndView modelAndView = new ModelAndView();
+            modelAndView.addObject("match", matchById);
+            modelAndView.addObject("finishRequest", finishRequest); // добавям ги защото ми трябват за "admin-finish" view
+            modelAndView.setViewName("admin-finish");
+
+              return modelAndView;
+         }
+
+
+
+//
+//         modelAndView.setViewName("redirect:/admin/matches"); -> тези не ми трябват, защото редиректвам към гет заявката
+//         modelAndView.addObject("match", matchById);
+//         modelAndView.addObject("finishRequest", finishRequest);
+
+        matchAdminService.finishMatch(id, finishRequest.getHomeGoals(), finishRequest.getAwayGoals());
+        return new ModelAndView("redirect:/admin/matches");
     }
 }
