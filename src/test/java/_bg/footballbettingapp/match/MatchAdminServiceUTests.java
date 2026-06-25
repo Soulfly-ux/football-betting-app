17e9db35e462a29cafa45df6b734610f337ef7cb
Soulfly-ux <sirenar3171@gmail.com>
2026-06-25 15:15:42 +0200

    test(match-admin): add unit tests for finishing matches

--- src/test/java/_bg/footballbettingapp/match/MatchAdminServiceUTests.java
+++ src/test/java/_bg/footballbettingapp/match/MatchAdminServiceUTests.java
@@ -1,23 +1,28 @@
 package _bg.footballbettingapp.match;
 
 import _bg.footballbettingapp.bet.service.BetService;
+import _bg.footballbettingapp.exception.DomainException;
 import _bg.footballbettingapp.match.model.Match;
 import _bg.footballbettingapp.match.model.MatchStatus;
 import _bg.footballbettingapp.match.repository.MatchRepository;
 import _bg.footballbettingapp.match.service.MatchAdminService;
 import _bg.footballbettingapp.team.service.TeamService;
 import _bg.footballbettingapp.user.service.UserService;
+import _bg.footballbettingapp.web.dto.EditMatchRequest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class MatchAdminServiceUTests {
@@ -72,7 +77,98 @@ public class MatchAdminServiceUTests {
         List<String> supportedLeagues = matchAdminService.getSupportedLeagues();
 
         assertEquals(leagues, supportedLeagues);
+    }
 
+
+    @Test
+    void givenMatch_whenMapToEditMatchRequest_thenReturnMappedDto() {
+
+        Match match = Match.builder()
+                .startTime(LocalDateTime.now())
+                .oddHome(BigDecimal.valueOf(2.10))
+                .oddDraw(BigDecimal.valueOf(3.00))
+                .oddAway(BigDecimal.valueOf(2.60))
+                .build();
+
+        EditMatchRequest result = matchAdminService.mapToEditMatchRequest(match);
+
+        assertEquals(match.getStartTime(), result.getStartTime());
+        assertEquals(match.getOddHome(), result.getOddHome());
+        assertEquals(match.getOddDraw(), result.getOddDraw());
+        assertEquals(match.getOddAway(), result.getOddAway());
+
+    }
+
+
+    @Test
+    void givenMissingMatch_whenFinishMatch_thenExceptionIsThrown() {
+
+        UUID id = UUID.randomUUID();
+        int homeGoals = 3;
+        int awayGoals = 0;
+
+       when(matchRepository.findById(id)).thenReturn(Optional.empty());
+
+       assertThrows(DomainException.class, () -> matchAdminService.finishMatch(id, homeGoals, awayGoals));
+       verify(matchRepository, never()).save(any());
+       verify(betService, never()).settleBetForMatch(any());
+
+    }
+
+
+    @Test
+    void givenFinishedMatch_whenFinishMatch_thenExceptionIsThrown() {
+
+        UUID id = UUID.randomUUID();
+        int homeGoals = 3;
+        int awayGoals = 0;
+
+        Match match = Match.builder()
+                .matchStatus(MatchStatus.FINISHED)
+                .build();
+
+        when(matchRepository.findById(id)).thenReturn(Optional.of(match));
+
+        assertThrows(DomainException.class, () -> matchAdminService.finishMatch(id, homeGoals, awayGoals));
+        verify(matchRepository, never()).save(any());
+        verify(betService, never()).settleBetForMatch(any());
+
+    }
+
+    @Test
+    void givenCancelledMatch_whenFinishMatch_thenExceptionIsThrown() {
+        UUID id = UUID.randomUUID();
+        int homeGoals = 3;
+        int awayGoals = 0;
+
+        Match match = Match.builder()
+                .matchStatus(MatchStatus.CANCELLED)
+                .build();
+
+        when(matchRepository.findById(id)).thenReturn(Optional.of(match));
+
+        assertThrows(DomainException.class, () -> matchAdminService.finishMatch(id, homeGoals, awayGoals));
+        verify(matchRepository, never()).save(any());
+        verify(betService, never()).settleBetForMatch(any());
+
+    }
+
+    @Test
+    void givenMatchNotInProgress_whenFinishMatch_thenExceptionIsThrown() {
+
+        UUID id = UUID.randomUUID();
+        int homeGoals = 3;
+        int awayGoals = 0;
+
+        Match match = Match.builder()
+                .matchStatus(MatchStatus.SCHEDULED)
+                .build();
+
+        when(matchRepository.findById(id)).thenReturn(Optional.of(match));
+
+        assertThrows(DomainException.class, () -> matchAdminService.finishMatch(id, homeGoals, awayGoals));
+        verify(matchRepository, never()).save(any());
+        verify(betService, never()).settleBetForMatch(any());
 
     }
 }
